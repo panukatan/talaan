cd7ee0538e8a3af287721e951c33a20c226619ea
Ernest Guevarra <ernest@guevarra.io>
2024-03-31 14:58:58 +0100

    initial commit

---
+++ R/talaan.R
@@ -0,0 +1,15 @@
+#'
+#' An Interface to the Philippine Civil Registration and Vital Statistics Data
+#'
+#' The Philippine Statistics Authority (PSA) makes civil registration data
+#' publicly available through their monthly updates on numbers of births,
+#' deaths, and marriages released via their website. This package provides
+#' utilities for accessing and processing the civil registration and vital
+#' statistics data released by the PSA.
+#'
+#' @name talaan
+#' @docType package
+#' @keywords internal
+#'
+"_PACKAGE"
+
